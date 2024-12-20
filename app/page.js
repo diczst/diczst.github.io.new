07735724afe0e43f46111ed592019f3632c94dd5
dickyzuerst <dzuerst@gmail.com>
2024-12-21 00:57:22 +0700

    add h1 and it's style

--- app/page.js
+++ app/page.js
@@ -5,9 +5,13 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div>
-          <div>text</div>
-          <div>text</div>
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left"><span>Software Developer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span>Dicky Pratama</span>
+            </h1>
+          </div>
+          <div>photo</div>
         </div>
       </div>
     </section>
