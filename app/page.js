7ce5e19e04103452c1d96341e95ead019a1e3014
dickyzuerst <dzuerst@gmail.com>
2024-12-21 01:12:43 +0700

    add desc

--- app/page.js
+++ app/page.js
@@ -6,10 +6,14 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left"><span>Software Developer</span>
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
             <h1 className="h1">
-              Hello I'm <br /> <span>Dicky Pratama</span>
+              Hello I'm <br /> <span className="text-accent">Dicky Pratama</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at Full Stack Development and proficient in various technologies and programming languages.
+            </p>
           </div>
           <div>photo</div>
         </div>
